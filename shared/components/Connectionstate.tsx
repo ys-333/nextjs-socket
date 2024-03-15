ea0f5eff554f7f5f43c0868240a17c8b39fc552d
ys-333 <yashwantsing333@gmail.com>
2024-03-15 22:58:07 +0530

    fix:remove files

--- shared/components/Connectionstate.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-interface ConnectionStateProps {
-  isConnected: string
-}
-export function ConnectionState({ isConnected }: ConnectionStateProps) {
-  return <p>State: {'' + isConnected}</p>
-}
