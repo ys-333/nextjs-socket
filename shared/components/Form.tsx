c6fd7278e10bb5fe22b392a552236c21b2426ea8
ys-333 <yashwantsing333@gmail.com>
2024-03-16 00:29:05 +0530

    feat:config socket

--- shared/components/Form.tsx
+++ shared/components/Form.tsx
@@ -1,39 +1,55 @@
 'use client'
-import React, { useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import { socket } from '../services/socket'
+
 interface FormProps {}
 
 const Form: React.FC<FormProps> = () => {
-  const messageRef = useRef<any>('')
-  const clickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const messageRef = useRef<HTMLInputElement>(null) // Correctly typed useRef
+  const [messages, setMessages] = useState<string[]>([]) // Renamed "message" to "messages" for clarity
+
+  useEffect(() => {
+    socket.connect()
+    // Function to handle incoming messages from the server
+    const handleMessage = (message: string) => {
+      setMessages((prevMessages) => [message, ...prevMessages])
+    }
+
+    // Subscribe to 'message' event from the server when component mounts
+    socket.on('message', handleMessage)
+
+    // Unsubscribe from 'message' event when component unmounts
+    return () => {
+      socket.off('message', handleMessage)
+    }
+  }, []) // Empty dependency array ensures this effect runs only once on component mount
+
+  const sendMessage = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    console.log(messageRef.current.value)
-    messageRef.current.value = ''
+    const newMessage = messageRef.current?.value.trim()
+    if (newMessage) {
+      // Emit 'message' event to the server with the new message
+      socket.emit('message', newMessage)
+      // Clear input field after sending message
+      messageRef.current!.value = ''
+    }
   }
+
   return (
-    <form className="flex flex-col justify-center items-center border-2 border-neutral-800">
-      <input
-        style={{
-          border: '1px solid black',
-          padding: '1rem',
-          borderRadius: '8px',
-        }}
-        className="border-2"
-        type="text"
-        placeholder="Enter your message"
-        ref={messageRef}
-      />
-      <button
-        style={{
-          border: '1px solid black',
-          padding: '1rem',
-          borderRadius: '8px',
-          marginTop: '1rem',
-        }}
-        onClick={clickHandler}
-      >
-        Send Message
-      </button>
-    </form>
+    <div>
+      <ul>
+        {/* Render list of messages */}
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+      {/* Form to enter and submit new message */}
+      <form onSubmit={sendMessage}>
+        <input type="text" placeholder="Enter your message" ref={messageRef} />
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
   )
 }
+
 export default Form
