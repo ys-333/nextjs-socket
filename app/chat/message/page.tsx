c9236c77b75c5d9a9490a12b2d1c8cef737333fc
ys-333 <yashwantsing333@gmail.com>
2024-06-28 01:53:30 +0530

    hello

--- app/chat/message/page.tsx
+++ app/chat/message/page.tsx
@@ -1,12 +1,15 @@
-import Form from '@/shared/components/Form'
+import Form from "@/shared/components/Form";
 interface MessageInterfaceProps {}
 
 const Message: React.FC<MessageInterfaceProps> = () => {
+  {
+    /* <h1>Message Application</h1> */
+  }
   return (
     <>
       <h1>Message Application</h1>
       <Form />
     </>
-  )
-}
-export default Message
+  );
+};
+export default Message;
